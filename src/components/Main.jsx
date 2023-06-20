8f6ea900d3edd1fdeff08456d3970a9027188e16
Earl006 <earljoe06@gmail.com>
2023-06-20 21:55:55 +0300

    main

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
-// import { TypeAnimation } from 'react-type-animation';
-import { FaTwitter, FaLinkedin, FaTelegram, FaWhatsapp } from 'react-icons/fa';
+import { TypeAnimation } from 'react-type-animation';
+import { FaInstagram, FaFacebook, FaWhatsapp } from 'react-icons/fa';
+
 
 function Main() {
   return (
@@ -10,6 +11,51 @@ function Main() {
         src='images/back2.jpg'
         alt='Background'
       />
+       <div className="absolute inset-0 flex items-center justify-center px-4">
+        <div className="text-center">
+          <h1 className="text-white text-4xl font-bold mb-4">We are Rightmen Customz</h1>
+          <h2 className="text-white text-2xl lg:text-4xl">
+            Specialists in<br/>{' '}
+            <TypeAnimation
+              sequence={[
+                'Minor and Major collision repairs.',
+                3000,
+                'Customization and Restoration.',
+                3000,
+                'Uphoslstery and detailed interiors.',
+                3000,
+                'Body and Rims respray.',
+                3000,
+                'Perfomance Tuning.',
+                3000,
+              ]}
+              wrapper="div"
+              speed={50}
+              style={{
+                display: 'inline-block',
+                fontSize: '1em',
+                paddingLeft: '5px',
+                color: 'orange',
+                fontWeight: 'bold',
+                fontStyle: 'italic',
+                textShadow: '2px 2px 4px rgba(0, 0, 0, 0.5)'
+              }}
+              repeat={Infinity}
+            />
+          </h2>
+          <div className="flex items-center justify-center mt-4">
+      <a href="https://m.facebook.com/edeninvestment" target="_blank" rel="noopener noreferrer">
+        <FaFacebook className="cursor-pointer text-white text-2xl lg:text-4xl mr-4" size={20} />
+      </a>
+      <a href="https://www.instagram.com/rightmencustomz/" target="_blank" rel="noopener noreferrer">
+        <FaInstagram className="cursor-pointer text-white text-2xl lg:text-4xl mr-4" size={20} />
+      </a>
+      <a href="https://wa.me/+254726616929" target="_blank" rel="noopener noreferrer">
+        <FaWhatsapp className="cursor-pointer text-white text-2xl lg:text-4xl mr-4" size={20} />
+      </a>
+    </div>
+        </div>
+      </div>
     </div>
   );
 }
--- src/components/Sidenav.jsx
+++ src/components/Sidenav.jsx
@@ -19,7 +19,7 @@ function Sidenav() {
                     </a>
                      <a onClick={handleNav} href="#about" className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100  m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200'>
                             <BsPerson size={20} />
-                        <span className='pl-4'>About Me</span>
+                        <span className='pl-4'>About Us</span>
                     </a>
                      <a onClick={handleNav} href="#projects" className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100  m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200'>
                         <AiOutlineProject size={20} />
